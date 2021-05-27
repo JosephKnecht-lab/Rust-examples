203d6b97ded375672c5eb873d89d6234c9980b41
Joseph Knecht <authorJosephKnecht@gmail.com>
2021-05-27 13:43:49 +0200

    update main

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,3 +1,11 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();  //Returns Result<T, E>
+
+    for stream in listener.incoming() {
+        let stream = stream umwarap();
+
+        println("Connection has been established!");
+    }
 }
