b092c4efd0975f2b49a5b90cafc9c701ff6d3b1a
JosephKnecht <authorJosephKnecht@gmail.com>
2021-06-04 10:40:11 +0200

    create lib

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,24 +1,8 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
 
-struct Config {
-    query: String,
-    filename: String,
-}
+use minigrep::Config;
 
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
-}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -31,18 +15,10 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
 
         process::exit(1);
     }}
 
-fn run(config: Config) -> Result<(), Box<dyn Error>>{
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong reading the file");
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
 
