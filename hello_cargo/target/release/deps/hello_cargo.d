eeee8b7e097b9cc5d644eca4c01b08b31a06cf6b
JosephKnecht <authorJosephKnecht@gmail.com>
2021-06-14 16:19:22 +0200

    build --release

---
+++ hello_cargo/target/release/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+/Users/igorstojanov/code/github-JK-lab/rust-examples/hello_cargo/target/release/deps/hello_cargo: src/main.rs
+
+/Users/igorstojanov/code/github-JK-lab/rust-examples/hello_cargo/target/release/deps/hello_cargo.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/igorstojanov/code/github-JK-lab/rust-examples/hello_cargo/target/release/hello_cargo: /Users/igorstojanov/code/github-JK-lab/rust-examples/hello_cargo/src/main.rs
