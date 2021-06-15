fac024f03b3faacb1bcaf37657ac6dfd5aef5c47
JosephKnecht <authorJosephKnecht@gmail.com>
2021-06-15 09:33:57 +0200

    shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -8,4 +8,16 @@ fn main() {
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+
+    shadowing();
+}
+
+fn shadowing(){
+    let x = 5;
+
+    let x = x + 1;
+
+    let x = x * 2;
+
+    println!("The value of x is: {}", x); 
 }
