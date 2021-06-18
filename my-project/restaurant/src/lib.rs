826651877a8bb0bc855b1f2454179630f386ed9f
JosephKnecht <authorJosephKnecht@gmail.com>
2021-06-18 13:20:07 +0200

    pub struct

--- my-project/restaurant/src/lib.rs
+++ my-project/restaurant/src/lib.rs
@@ -15,6 +15,20 @@ mod front_of_house {
 }
 
 mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("Peaches"),
+            }
+        }
+    }
+
     fn fix_incorrect_order(){
         cook_order();
         super::serve_order();
@@ -28,14 +42,21 @@ pub fn eat_at_restaurant(){
     crate::front_of_house::hosting::add_to_waitlist();
 
     front_of_house::hosting::add_to_waitlist();
+
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
 }
 
 fn serve_order(){
 
 }
-
-
-
 
 
 #[cfg(test)]
