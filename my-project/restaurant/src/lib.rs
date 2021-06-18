3f1baa65d2360271eb9ad86c09f18734ce98c82b
JosephKnecht <authorJosephKnecht@gmail.com>
2021-06-18 12:58:57 +0200

    paths

--- my-project/restaurant/src/lib.rs
+++ my-project/restaurant/src/lib.rs
@@ -15,6 +15,12 @@ mod front_of_house {
 }
 
 
+pub fn eat_at_restaurant(){
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    front_of_house::hosting::add_to_waitlist();
+}
+
 
 
 #[cfg(test)]
