d6dc4cb7aa38ed2ebd6789d959a9e7894f41a1b1
JosephKnecht <authorJosephKnecht@gmail.com>
2021-06-18 12:16:18 +0200

    pattern matching _

--- matching/src/main.rs
+++ matching/src/main.rs
@@ -44,4 +44,17 @@ fn main() {
     let six = plus_one(five);
     let none = plus_one(None);
     println!("Plus one values: {:?}", six);
+
+
+    let some_u8_value = 7;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (),
+    }
+
+    
+    
 }
