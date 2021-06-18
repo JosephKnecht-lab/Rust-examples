303e89bffeb00e3269dae3aef802e3406c3cc1af
JosephKnecht <authorJosephKnecht@gmail.com>
2021-06-18 15:37:31 +0200

    use external crate

--- my-project/src/main.rs
+++ my-project/src/main.rs
@@ -3,18 +3,23 @@ use std::io::Result as IoResult;
 use std::fmt::Result;
 use std::collections::HashMap;
 
+use rand::Rng;
+
 
 fn main() {
     let mut map = HashMap::new();
     map.insert(1, 2);
 
     println!("Map is: {:?}", map);
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
 }
 
-fn function1() -> Result {
-    // --snip--
-}
+// fn function1() -> Result {
+//     // --snip--
+// }
 
-fn function2() -> IoResult<()> {
-    // --snip--
-}
+// fn function2() -> IoResult<()> {
+//     // --snip--
+// }
