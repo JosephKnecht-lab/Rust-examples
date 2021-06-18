04a9a86b22205873041aa5f295129f96358d77c5
JosephKnecht <authorJosephKnecht@gmail.com>
2021-06-18 15:27:59 +0200

    different namespaces

--- my-project/src/main.rs
+++ my-project/src/main.rs
@@ -1,8 +1,20 @@
+
+use std::io;
+use std::fmt;
 use std::collections::HashMap;
+
 
 fn main() {
     let mut map = HashMap::new();
     map.insert(1, 2);
 
     println!("Map is: {:?}", map);
+}
+
+fn function1() -> fmt::Result {
+    // --snip--
+}
+
+fn function2() -> io::Result<()> {
+    // --snip--
 }
