2fe894076c06c9e1ae8f9425c49457d30f8b0989
JosephKnecht <authorJosephKnecht@gmail.com>
2021-06-18 11:01:41 +0200

    expend enum

--- matching/src/main.rs
+++ matching/src/main.rs
@@ -1,8 +1,16 @@
+// #[derive(Debug)] // so we can inspect the state in a minute
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
+}
+
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -13,12 +21,15 @@ fn value_in_cents(coin: Coin) -> u8 {
         },
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            // println!("State quarter from {:?}!", state);
+            25
+        },
     }
 }
 
 fn main() {
-    let myCoin = Coin::Penny;
+    let myCoin = Coin::Quarter(UsState::Alaska);
     let value: u8 = value_in_cents(myCoin);
     println!("Value is : {}", value);
 }
