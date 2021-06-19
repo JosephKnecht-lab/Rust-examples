b5b1562cb8d92b97281ff11dd77989656eb88c36
JosephKnecht <authorJosephKnecht@gmail.com>
2021-06-19 16:04:07 +0200

    append separate strings

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -116,5 +116,10 @@ fn append_strings(){
 
     println!("Appended string :{}",s);
 
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s2 is {}", s2);
+
 
 }
