9b1f88e52ceea8cf0b3f209ad91062162c2a7307
JosephKnecht <authorJosephKnecht@gmail.com>
2021-06-19 17:35:24 +0200

    add words to hashmap

--- hashmap/src/main.rs
+++ hashmap/src/main.rs
@@ -22,6 +22,7 @@ fn main() {
 
     create();
     ownership();
+    entry_value();
 
 }
 
@@ -46,4 +47,25 @@ fn ownership(){
     map.insert(field_name, field_value);
     // field_name and field_value are invalid at this point, try using them and
     // see what compiler error you get!
+}
+
+fn entry_value(){
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    println!("{:?}", scores);
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
 }
