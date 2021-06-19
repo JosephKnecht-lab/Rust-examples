68dcdf6b5c760fb24fd822ab2957af264972a5ae
JosephKnecht <authorJosephKnecht@gmail.com>
2021-06-19 17:13:37 +0200

    ownership in hashmap

--- hashmap/src/main.rs
+++ hashmap/src/main.rs
@@ -11,6 +11,7 @@ fn main() {
     println!("Scores: {:?}",scores);
 
     create();
+    ownership();
 
 }
 
@@ -24,5 +25,15 @@ fn create(){
 
         println!("Scores: {:?}",scores);
 
+}
 
+fn ownership(){
+
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    // field_name and field_value are invalid at this point, try using them and
+    // see what compiler error you get!
 }
