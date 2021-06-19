186c16c35f8d57da691311279ef1d3d32e0d855b
JosephKnecht <authorJosephKnecht@gmail.com>
2021-06-19 16:11:08 +0200

    format macro

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -130,4 +130,13 @@ fn append_strings(){
 
     println!("s3 is {}", s3);
 
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = format!("{}-{}-{}", s1, s2, s3);
+
+    println!("s is {}", s);
+
+
 }
