09913829f647b5786ee6a50955a8385ba15aedc6
JosephKnecht <authorJosephKnecht@gmail.com>
2021-06-19 17:08:20 +0200

    create from vectors

--- hashmap/src/main.rs
+++ hashmap/src/main.rs
@@ -10,4 +10,19 @@ fn main() {
 
     println!("Scores: {:?}",scores);
 
+    create();
+
+}
+
+fn create(){
+
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![100, 500];
+
+    let mut scores: HashMap<_, _> =
+        teams.into_iter().zip(initial_scores.into_iter()).collect();
+
+        println!("Scores: {:?}",scores);
+
+
 }
