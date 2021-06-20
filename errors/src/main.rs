bc5a98d5be3d47f88b297bbb5b9c290ede5e7b28
JosephKnecht <authorJosephKnecht@gmail.com>
2021-06-20 18:35:41 +0200

    return error

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,5 +1,7 @@
 use std::fs::File;
 use std::io::ErrorKind;
+use std::io;
+use std::io::Read;
 
 
 fn main() {
@@ -20,6 +22,9 @@ fn main() {
 
     unwrap_test();
     expect_test();
+    let r = read_username_from_file();
+    println!("Result is is: {:?}",r);
+
 }
 
 fn unwrap_test() {
@@ -30,4 +35,20 @@ fn unwrap_test() {
 fn expect_test(){
     let f = File::open("hello.txt").expect("Failed to open hello.txt");
     println!("f is: {:?}",f);
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
 }
