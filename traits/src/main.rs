3c653d846a69f7e2d3c7f824a7a8c7d7da5dfa69
JosephKnecht <authorJosephKnecht@gmail.com>
2021-06-21 11:43:15 +0200

    passing trait as parameter

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -5,9 +5,9 @@ pub struct NewsArticle {
     pub content: String,
 }
 
-impl Summary for NewsArticle {
-    
-}
+// impl Summary for NewsArticle {
+
+// }
 
 pub struct Tweet {
     pub username: String,
@@ -17,15 +17,17 @@ pub struct Tweet {
 }
 
 impl Summary for Tweet {
-    fn summarize(&self) -> String {
-        format!("{}: {}", self.username, self.content)
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
     }
 }
 
 
 pub trait Summary {
+    fn summarize_author(&self) -> String;
+
     fn summarize(&self) -> String {
-        String::from("(Read more...)")
+        format!("(Read more from {}...)", self.summarize_author())
     }
 }
 
@@ -41,6 +43,8 @@ fn main() {
 
     println!("1 new tweet: {}", tweet.summarize());
 
+    println!("1 new tweet: {}", tweet.summarize());
+
     let article = NewsArticle {
         headline: String::from("Penguins win the Stanley Cup Championship!"),
         location: String::from("Pittsburgh, PA, USA"),
@@ -51,6 +55,11 @@ fn main() {
         ),
     };
 
-    println!("New article available! {}", article.summarize());
+    notify(&tweet);
+    // println!("New article available! {}", article.summarize());
 
+}
+
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
 }
