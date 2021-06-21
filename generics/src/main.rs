df9341a5193d6e3383624388aa3a95706cd22da2
JosephKnecht <authorJosephKnecht@gmail.com>
2021-06-21 12:05:52 +0200

    PartialOrd + Copy

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -36,10 +36,6 @@ fn main() {
 
     let result = largest(&char_list);
     println!("The largest char is {}", result);
-
-    let both_integer = Point { x: 5, y: 10 };
-    let both_float = Point { x: 1.0, y: 4.0 };
-    let integer_and_float = Point { x: 5, y: 4.0 };
 }
 
 fn largest_i32(list: &[i32]) -> i32 {
@@ -66,7 +62,7 @@ fn largest_char(list: &[char]) -> char {
     largest
 }
 
-fn largest<T>(list: &[T]) -> T {
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
     let mut largest = list[0];
 
     for &item in list {
