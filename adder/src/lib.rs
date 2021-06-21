0d81225697eb1748ba184b9441d80718df16c5dd
JosephKnecht <authorJosephKnecht@gmail.com>
2021-06-21 14:38:39 +0200

    rectanble test

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -13,6 +13,8 @@ impl Rectangle {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn test1() {
         assert_eq!(2 + 2, 4);
@@ -21,6 +23,21 @@ mod tests {
 
     #[test]
     fn another() {
-        panic!("test fails");
+        // panic!("test fails");
+    }
+
+    #[test]
+    fn large_can_hold_small(){
+        let larger = Rectangle {
+            width: 7,
+            height: 8,
+        };
+
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
     }
 }
