8e35567440e8b587d76e60c2db6c10389dfa8d11
JosephKnecht <authorJosephKnecht@gmail.com>
2021-06-23 18:14:12 +0200

    add button and selectbox

--- oop/src/main.rs
+++ oop/src/main.rs
@@ -17,6 +17,30 @@ where
     }
 }
 
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        // code to actually draw a button
+    }
+}
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // code to actually draw a select box
+    }
+}
+
 
 
 pub struct AveragedCollection {
