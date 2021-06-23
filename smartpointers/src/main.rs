49d483d7dc33e8f69ef2d1a4125edeb1acf523a9
JosephKnecht <authorJosephKnecht@gmail.com>
2021-06-23 09:43:35 +0200

    value used after moved

--- smartpointers/src/main.rs
+++ smartpointers/src/main.rs
@@ -8,6 +8,13 @@ impl Drop for CustomSmartPointer {
     }
 }
 
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+
 
 fn main() {
     let c = CustomSmartPointer {
@@ -17,4 +24,8 @@ fn main() {
         data: String::from("other stuff"),
     };
     println!("CustomSmartPointers created.");
+
+    let a = Cons(5, Box::new(Cons(10, Box::new(Nil))));
+    let b = Cons(3, Box::new(a));
+    let c = Cons(4, Box::new(a));
 }
