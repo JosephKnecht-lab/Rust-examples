08fb5e7cdc0a7404c192f8acdaed4a63eb4f5628
JosephKnecht <authorJosephKnecht@gmail.com>
2021-06-24 13:02:06 +0200

    unsafe traits

--- unsaferust/src/main.rs
+++ unsaferust/src/main.rs
@@ -10,6 +10,13 @@ use std::slice;
 static HELLO_WORLD: &str = "Hello, world!";
 static mut COUNTER: u32 = 0;
 
+unsafe trait Foo {
+    // methods go here
+}
+
+unsafe impl Foo for i32 {
+    // method implementations go here
+}
 
 
 fn main() {
