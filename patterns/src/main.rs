1569271babe752205f1263e261ea220ae4bd60a9
JosephKnecht <authorJosephKnecht@gmail.com>
2021-06-24 11:05:53 +0200

    guard from outside scope

--- patterns/src/main.rs
+++ patterns/src/main.rs
@@ -275,4 +275,15 @@ fn match_guard(){
         Some(x) => println!("{}", x),
         None => (),
     }
+
+    let x = Some(10);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(n) if n == y => println!("Matched, n = {}", n),  //y is the outside y 
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {}", x, y);
 }
