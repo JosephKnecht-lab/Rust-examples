0fbc78d38f7669428a997e1b1be4ad879c5c7899
JosephKnecht <authorJosephKnecht@gmail.com>
2021-06-24 12:31:50 +0200

    dereferebce raw pointer with unsafe block

--- unsaferust/src/main.rs
+++ unsaferust/src/main.rs
@@ -1,3 +1,28 @@
+// With unsfafe you can..
+// Dereference a raw pointer
+// Call an unsafe function or method
+// Access or modify a mutable static variable
+// Implement an unsafe trait
+// Access fields of unions
+
+
 fn main() {
-    println!("Hello, world!");
+    derefence_raw_poointer();
+}
+
+fn derefence_raw_poointer(){
+    let mut num = 5;
+
+    let r1 = &num as *const i32;
+    let r2 = &mut num as *mut i32;
+
+    // println!("r1 is: {}", *r1);  //cant dereference r1 here
+
+    unsafe {
+        println!("r1 is: {}", *r1);
+        println!("r2 is: {}", *r2);
+    }
+ 
+    let address = 0x012345usize;  //point to exact location in memory
+    let r = address as *const i32;
 }
