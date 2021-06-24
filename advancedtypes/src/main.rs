b568b7265a8b255ea30f8286c22453b652d4452e
JosephKnecht <authorJosephKnecht@gmail.com>
2021-06-24 20:22:24 +0200

    alias

--- advancedtypes/src/main.rs
+++ advancedtypes/src/main.rs
@@ -1,3 +1,26 @@
+use std::fmt;
+use std::io::Error;
+
+// pub trait Write {
+//     fn write(&mut self, buf: &[u8]) -> Result<usize, Error>;
+//     fn flush(&mut self) -> Result<(), Error>;
+
+//     fn write_all(&mut self, buf: &[u8]) -> Result<(), Error>;
+//     fn write_fmt(&mut self, fmt: fmt::Arguments) -> Result<(), Error>;
+// }
+
+type Result<T> = std::result::Result<T, std::io::Error>;  //the two traits are equvalant, since we create a new alias for Result T 
+
+
+pub trait Write {
+    fn write(&mut self, buf: &[u8]) -> Result<usize>;
+    fn flush(&mut self) -> Result<()>;
+
+    fn write_all(&mut self, buf: &[u8]) -> Result<()>;
+    fn write_fmt(&mut self, fmt: fmt::Arguments) -> Result<()>;
+}
+
+
 fn main() {
     
     type Kilometers = i32;  //type alliases Kilometers is i32 now
