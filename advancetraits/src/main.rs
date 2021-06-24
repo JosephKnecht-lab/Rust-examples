aa3db012038882f429e605bdfdc3a68f34949faa
JosephKnecht <authorJosephKnecht@gmail.com>
2021-06-24 15:22:35 +0200

    wrapper struct

--- advancetraits/src/main.rs
+++ advancetraits/src/main.rs
@@ -2,6 +2,14 @@
 use std::ops::Add;
 use std::fmt;
 
+struct Wrapper(Vec<String>);
+
+impl fmt::Display for Wrapper {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[{}]", self.0.join(", "))
+    }
+}
+
 trait OutlinePrint: fmt::Display {
     fn outline_print(&self) {
         let output = self.to_string();
@@ -128,5 +136,6 @@ fn main() {
     print.outline_print();
 
 
-
+    let w = Wrapper(vec![String::from("hello"), String::from("world")]);
+    println!("w = {}", w);
 }
