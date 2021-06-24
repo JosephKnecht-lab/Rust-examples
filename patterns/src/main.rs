172a44f622ce1197745ab9736e977188397033e7
JosephKnecht <authorJosephKnecht@gmail.com>
2021-06-24 10:01:57 +0200

    matching literals

--- patterns/src/main.rs
+++ patterns/src/main.rs
@@ -43,6 +43,16 @@ fn main() {
         println!("{}", x);
     };
 
+    //matching literals
+    let x = 1;
+
+    match x {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+
 }
 
 fn print_coordinates(&(x, y): &(i32, i32)) {
