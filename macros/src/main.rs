87cf3b4ca1963abad0ca602bf2ef755b5c9e0fc4
JosephKnecht <authorJosephKnecht@gmail.com>
2021-06-25 10:48:54 +0200

    procedural macro

--- macros/src/main.rs
+++ macros/src/main.rs
@@ -1,6 +1,8 @@
 // Custom #[derive] macros that specify code added with the derive attribute used on structs and enums
 // Attribute-like macros that define custom attributes usable on any item
 // Function-like macros that look like function calls but operate on the tokens specified as their argument
+use proc_macro;
+
 
 #[macro_export]
 macro_rules! vec {    // vec! macro definition
@@ -15,9 +17,15 @@ macro_rules! vec {    // vec! macro definition
     };
 }
 
+// #[some_attribute]        example procedural macro.. Taje 
+// pub fn some_name(input: TokenStream) -> TokenStream {
+// }
+
 
 fn main() {
     let v: Vec<u32> = vec![1, 2, 3];  // simple vec! macro
 
 
 }
+
+
