72ac56b46efa79ae878ee7d3b6dacdb885fb1d9e
JosephKnecht <authorJosephKnecht@gmail.com>
2021-06-25 10:12:59 +0200

    cant return closure

--- advancedfunctions/src/main.rs
+++ advancedfunctions/src/main.rs
@@ -6,8 +6,24 @@ fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {  //use fn to pass a function a
     f(arg) + f(arg)
 }
 
+enum Status {
+    Value(u32),
+    Stop,
+}
+
 fn main() {
     let answer = do_twice(add_one, 5);
 
     println!("The answer is: {}", answer);
+
+    let list_of_numbers = vec![1, 2, 3];
+    let list_of_strings: Vec<String> =
+        list_of_numbers.iter().map(|i| i.to_string()).collect();
+
+    let list_of_statuses: Vec<Status> = (0u32..20).map(Status::Value).collect();
+
+}
+
+fn returns_closure() -> dyn Fn(i32) -> i32 {  //cant return a closure directly
+    |x| x + 1
 }
