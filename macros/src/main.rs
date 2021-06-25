4a497f6b647a360f28d5339d467422db3cfab913
JosephKnecht <authorJosephKnecht@gmail.com>
2021-06-25 10:24:35 +0200

    vec macro

--- macros/src/main.rs
+++ macros/src/main.rs
@@ -1,3 +1,23 @@
+// Custom #[derive] macros that specify code added with the derive attribute used on structs and enums
+// Attribute-like macros that define custom attributes usable on any item
+// Function-like macros that look like function calls but operate on the tokens specified as their argument
+
+#[macro_export]
+macro_rules! vec {    // vec! macro definition
+    ( $( $x:expr ),* ) => {
+        {
+            let mut temp_vec = Vec::new();
+            $(
+                temp_vec.push($x);
+            )*
+            temp_vec
+        }
+    };
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let v: Vec<u32> = vec![1, 2, 3];  // simple vec! macro
+
+
 }
