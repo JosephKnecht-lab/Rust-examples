cf7469a696a29cf7a91f77e97a97ca58fd7844df
Joseph Knecht <83087510+JosephKnecht-lab@users.noreply.github.com>
2021-11-04 15:17:10 +0100

    removes target folder

--- hello_cargo/target/release/deps/hello_cargo.d
+++
@@ -1,5 +0,0 @@
-/Users/igorstojanov/code/github-JK-lab/rust-examples/hello_cargo/target/release/deps/hello_cargo: src/main.rs
-
-/Users/igorstojanov/code/github-JK-lab/rust-examples/hello_cargo/target/release/deps/hello_cargo.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/release/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/igorstojanov/code/github-JK-lab/rust-examples/hello_cargo/target/release/hello_cargo: /Users/igorstojanov/code/github-JK-lab/rust-examples/hello_cargo/src/main.rs
